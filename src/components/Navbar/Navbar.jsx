65ddb16e6f43bce078f0d4a3667da4103d80fa07
Luis Rodriguez <rodriguezfallasluis@gmail.com>
2022-01-25 12:11:24 -0600

    Nav Error: margin-boton, corrected

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,15 @@
-import React,{useState} from 'react';
-import {Container, Logo, LogoContainer, Menu, MenuItem, MenuItemLink, MobileIcon, Wrapper} from './Styles/NavbarElements'
+import React, { useState } from "react";
+import {
+  Container,
+  Logo,
+  LogoContainer,
+  Menu,
+  MenuItem,
+  MenuItemLink,
+  MobileIcon,
+  Wrapper,
+  NavText,
+} from "./Styles/NavbarElements";
 import {
   FaBars,
   FaTimes,
@@ -7,61 +17,71 @@ import {
   FaAddressCard,
   FaBlog,
   FaUserTie,
- } from "react-icons/fa";
-import {IconContext} from 'react-icons'
-
+} from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
-  
+
   return (
     <>
       <Container>
         <Wrapper>
-          <IconContext.Provider value={{style:{fontSize: "1.5em"}}}>
-            <LogoContainer  to="/">
-              <Logo src="../img/yovector.svg" className='logo' alt="" />
-              <p>Luis</p>
-              <p>Rodríguez</p>
+          <IconContext.Provider value={{ style: { fontSize: "1.5em" } }}>
+            <LogoContainer to="/">
+              <Logo src="../img/yovector.svg" className="logo" alt="" />
+              <NavText>Luis</NavText>
+              <NavText>Rodríguez</NavText>
             </LogoContainer>
 
-            <MobileIcon onClick={()=>setShowMobileMenu(!showMobileMenu)}>
-              {showMobileMenu ? <FaTimes/> : <FaBars/>}
-            
+            <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+              {showMobileMenu ? <FaTimes /> : <FaBars />}
             </MobileIcon>
 
             <Menu open={showMobileMenu}>
               <MenuItem>
-                <MenuItemLink to="/" onClick={()=>setShowMobileMenu(!showMobileMenu)}>
+                <MenuItemLink
+                  to="/"
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
                   <div>
-                    <FaUserTie/>
+                    <FaUserTie />
                     About me
                   </div>
                 </MenuItemLink>
               </MenuItem>
 
               <MenuItem>
-                <MenuItemLink to="/blog" onClick={()=>setShowMobileMenu(!showMobileMenu)}>
-                <div>
-                    <FaBlog/>
+                <MenuItemLink
+                  to="/blog"
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <div>
+                    <FaBlog />
                     Blog
                   </div>
                 </MenuItemLink>
               </MenuItem>
 
               <MenuItem>
-                <MenuItemLink to="/portfolio" onClick={()=>setShowMobileMenu(!showMobileMenu)}>
-                <div>
-                    <FaPenNib/>
+                <MenuItemLink
+                  to="/portfolio"
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <div>
+                    <FaPenNib />
                     Portfolio
                   </div>
                 </MenuItemLink>
               </MenuItem>
 
               <MenuItem>
-                <MenuItemLink to="/contact" onClick={()=>setShowMobileMenu(!showMobileMenu)}>
-                <div>
-                    <FaAddressCard/>
+                <MenuItemLink
+                  to="/contact"
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <div>
+                    <FaAddressCard />
                     Contact
                   </div>
                 </MenuItemLink>
@@ -71,7 +91,7 @@ function Navbar() {
         </Wrapper>
       </Container>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
