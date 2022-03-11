670b9e79bb6680174c182e0115cd373f6f87d605
Luis Rodriguez <rodriguezfallasluis@gmail.com>
2022-03-11 13:59:43 -0600

    Image optimization

--- src/pages/Aboutme/About.jsx
+++ src/pages/Aboutme/About.jsx
@@ -20,6 +20,7 @@ import {
 // import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
 // import { Animated } from "react-animated-css";
 import WOW from "wowjs";
+import data from "../../store/technologyData";
 
 new WOW.WOW().init();
 
@@ -33,11 +34,9 @@ function About() {
           {/* <YoutubeEmbed embedId="58479"/> */}
           <FotoSkills
             className="wow zoomIn"
-            data-wow-offset="3"
-            data-wow-iteration="3"
             data-wow-duration="2s"
-            data-wow-delay="1.5s"
-            src="../img/SoftSkills.png"
+            data-wow-delay="1s"
+            src="../img/SoftSkills.webp"
           ></FotoSkills>
           <AboutContainer>
             <AboutSubTitle>Who I am?</AboutSubTitle>
@@ -77,103 +76,23 @@ function About() {
               projects:
             </NavParagraph>
             <ContainerStikers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/html5.png" />
-                <AboutLink
-                  href="https://developer.mozilla.org/es/docs/Web/HTML"
-                  target="_blank"
-                >
-                  Html
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/css3.png" />
-                <AboutLink
-                  href="https://developer.mozilla.org/es/docs/Web/CSS"
-                  target="_blank"
-                >
-                  Css3
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/js.png" />
-                <AboutLink
-                  href="https://developer.mozilla.org/es/docs/Web/JavaScript"
-                  target="_blank"
-                >
-                  JavaScript
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/java.png" />
-                <AboutLink
-                  href="https://www.java.com/es/download/help/whatis_java.html"
-                  target="_blank"
-                >
-                  Java
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/git.png" />
-                <AboutLink href="https://github.com/" target="_blank">
-                  GitHub
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/bootstrap.png" />
-                <AboutLink href="https://getbootstrap.com/" target="_blank">
-                  Boostrap
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/react.png" />
-                <AboutLink href="https://es.reactjs.org/" target="_blank">
-                  React
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/sc.png" />
-                <AboutLink
-                  href="https://styled-components.com/"
-                  target="_blank"
-                >
-                  Styled Components
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/MySQL.png" />
-                <AboutLink href="https://www.mysql.com/" target="_blank">
-                  MySQL
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/mongodb.png" />
-                <AboutLink href="https://www.mongodb.com/" target="_blank">
-                  MongoDB
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/vs.png" />
-                <AboutLink
-                  href="https://code.visualstudio.com/"
-                  target="_blank"
-                >
-                  Visual Studio
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/nb.png" />
-                <AboutLink href="https://netbeans.apache.org/" target="_blank">
-                  NetBeans
-                </AboutLink>
-              </WrapStickers>
+              {data.programmingLanguagesData.map((item, index) => {
+                return (
+                  <WrapStickers>
+                    <FotoStikers src={item.img} />
+                    <AboutLink href={item.url} target="_blank">
+                      {item.name}
+                    </AboutLink>
+                  </WrapStickers>
+                );
+              })}
             </ContainerStikers>
           </AboutSkillsContainer>
           <FotoSkills
             className="wow zoomIn"
             data-wow-duration="2s"
             data-wow-delay="1s"
-            src="../img/SkillsBrain.png"
+            src="../img/SkillsBrain.webp"
           ></FotoSkills>
         </AboutSkillSection>
 
@@ -182,10 +101,12 @@ function About() {
             className="wow zoomIn"
             data-wow-duration="2s"
             data-wow-delay="1.5s"
-            src="../img/HandDigital.png"
-          ></FotoSkills>
+            src="../img/HandDigital.webp"
+          />
 
           <AboutSkillsContainer>
+            <AboutSubTitle>Others software like.</AboutSubTitle>
+            <br />
             <NavParagraph>
               In graphics, videos, sound and web software:
             </NavParagraph>
@@ -195,54 +116,16 @@ function About() {
               Figma.
             </NavParagraph>
             <ContainerStikers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/ai.png" />
-                <AboutLink
-                  href="https://www.adobe.com/la/products/illustrator.html"
-                  target="_blank"
-                >
-                  Illustrator
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/ps.png" />
-                <AboutLink
-                  href="https://www.adobe.com/la/products/photoshop.html"
-                  target="_blank"
-                >
-                  Photoshop
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/xd.png" />
-                <AboutLink
-                  href="https://www.adobe.com/la/products/xd.html"
-                  target="_blank"
-                >
-                  AdobeXD
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/figma.png" />
-                <AboutLink href="https://www.figma.com/" target="_blank">
-                  Figma
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/filmora.png" />
-                <AboutLink
-                  href="https://filmora.wondershare.net/es/filmora9-video-editor-es.html"
-                  target="_blank"
-                >
-                  Filmora
-                </AboutLink>
-              </WrapStickers>
-              <WrapStickers>
-                <FotoStikers src="../stikers/audacity.png" />
-                <AboutLink href="https://www.audacityteam.org/" target="_blank">
-                  Audacity
-                </AboutLink>
-              </WrapStickers>
+              {data.softwareData.map((item, index) => {
+                return (
+                  <WrapStickers>
+                    <FotoStikers src={item.img} />
+                    <AboutLink href={item.url} target="_blank">
+                      {item.name}
+                    </AboutLink>
+                  </WrapStickers>
+                );
+              })}
             </ContainerStikers>
           </AboutSkillsContainer>
         </AboutSkillSection>
---
+++ src/store/technologyData.js
@@ -0,0 +1,135 @@
+//Images
+import {
+  technologyDataHtml,
+  technologyDataCss,
+  technologyDataJs,
+  technologyDataGitHub,
+  technologyDataBootstrap,
+  technologyDataReact,
+  technologyDataSc,
+  technologyDataMysql,
+  technologyDataVS,
+  technologyDataSB,
+  technologyDataIllustrator,
+  technologyDataPhotoshop,
+  technologyDataXd,
+  technologyDataAfterEffects,
+  technologyDataAudacity,
+  technologyDataFilmora,
+  technologyDataFigma
+} from './index'
+
+//Data
+const data = {
+  programmingLanguagesData: [
+    {
+      id: 1,
+      name: "Html",
+      img: technologyDataHtml,
+      url: "https://developer.mozilla.org/es/docs/Web/HTML"
+    },
+    {
+      id: 2,
+      name: "Css",
+      img: technologyDataCss,
+      url: "https://developer.mozilla.org/es/docs/Web/CSS"
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      img: technologyDataJs,
+      url: "https://developer.mozilla.org/es/docs/Web/JavaScript"
+    },
+    {
+      id: 4,
+      name: "GitHub",
+      img: technologyDataGitHub,
+      url: "https://github.com/"
+    },
+    {
+      id: 5,
+      name: "Boostrap",
+      img: technologyDataBootstrap,
+      url: "https://getbootstrap.com/"
+    },
+    {
+      id: 6,
+      name: "React",
+      img: technologyDataReact,
+      url: "https://es.reactjs.org/"
+    },
+    {
+      id: 7,
+      name: "Styled Components",
+      img: technologyDataSc,
+      url: "https://styled-components.com/"
+    },
+    {
+      id: 8,
+      name: "MySQL",
+      img: technologyDataMysql,
+      url: "https://www.mysql.com/"
+    },
+    {
+      id: 9,
+      name: "Visual Studio Code",
+      img: technologyDataVS,
+      url: "https://code.visualstudio.com/"
+    },
+    {
+      id: 10,
+      name: "StoryBook",
+      img: technologyDataSB,
+      url: "https://getbootstrap.com/"
+    },
+
+  ],
+  softwareData: [
+    {
+      id: 1,
+      name: "Illustrator",
+      img: technologyDataIllustrator,
+      url: "https://www.adobe.com/la/products/illustrator.html"
+    },
+    {
+      id: 2,
+      name: "Photoshop",
+      img: technologyDataPhotoshop,
+      url: "https://www.adobe.com/la/products/photoshop.html"
+    },
+    {
+      id: 3,
+      name: "AdobeXD",
+      img: technologyDataXd,
+      url: "https://www.adobe.com/la/products/xd.html"
+    },
+    {
+      id: 4,
+      name: "After Effects",
+      img: technologyDataAfterEffects,
+      url: "https://www.adobe.com/la/products/aftereffects/campaign/pricing.html"
+    },
+    {
+      id: 5,
+      name: "Audacity",
+      img: technologyDataAudacity,
+      url: "https://www.audacityteam.org/"
+    },
+    {
+      id: 6,
+      name: "Filmora",
+      img: technologyDataFilmora,
+      url: "https://filmora.wondershare.net/es/filmora9-video-editor-es.html"
+    },
+    {
+      id: 7,
+      name: "Figma",
+      img: technologyDataFigma,
+      url: "https://www.figma.com/"
+    },
+  ]
+}
+// Export data
+export default data;
+
+
